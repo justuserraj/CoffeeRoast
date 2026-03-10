df80642b29a004e2c92c1f9033244026f37c7b14
[dyad] <justuserraj@gmail.com>
2026-03-10 05:38:36 +0530

    Updated src/components/PageLayout.tsx

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { MadeWithDyad } from './made-with-dyad';
+import { MadeWithVizoxStudio } from './made-with-dyad';
 
 interface PageLayoutProps {
   children: React.ReactNode;
@@ -27,7 +27,7 @@ const PageLayout = ({ children, title, subtitle }: PageLayoutProps) => {
         </div>
       </main>
       <Footer />
-      <MadeWithDyad />
+      <MadeWithVizoxStudio />
     </div>
   );
 };
